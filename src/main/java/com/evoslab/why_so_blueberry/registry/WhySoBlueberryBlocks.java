820b1f3baa73c92ecd9274b8b9c10a694a956069
Epic312 <kaden_ostendorf@yahoo.com>
2020-10-10 12:57:48 -0600

    blueberries (both kinds) + baskets & ice cream block

---
+++ src/main/java/com/evoslab/why_so_blueberry/registry/WhySoBlueberryBlocks.java
@@ -0,0 +1,37 @@
+package com.evoslab.why_so_blueberry.registry;
+
+import com.evoslab.why_so_blueberry.WhySoBlueberry;
+import com.teamabnormals.abnormals_core.core.utils.RegistryHelper;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.material.MaterialColor;
+import net.minecraft.item.ItemGroup;
+import net.minecraftforge.common.ToolType;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = WhySoBlueberry.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class WhySoBlueberryBlocks {
+    public static final RegistryHelper HELPER = WhySoBlueberry.REGISTRY_HELPER;
+
+    //TODO: Blueberry bush
+    //TODO: Blueberry cake
+
+    public static final RegistryObject<Block> BLUEBERRY_ICE_CREAM_BLOCK = HELPER.createBlock("blueberry_ice_cream_block", ()->new Block(Properties.BLUEBERRY_ICE_CREAM_BLOCK), ItemGroup.BUILDING_BLOCKS);
+
+    public static final RegistryObject<Block> BLUEBERRY_BASKET = HELPER.createCompatBlock("quark", "blueberry_basket", ()->new Block(Properties.BLUEBERRY_BASKET), ItemGroup.DECORATIONS);
+    public static final RegistryObject<Block> PINK_BLUEBERRY_BASKET = HELPER.createCompatBlock("quark", "pink_blueberry_basket", ()->new Block(Properties.PINK_BLUEBERRY_BASKET), ItemGroup.DECORATIONS);
+
+    static class Properties {
+        public static final AbstractBlock.Properties BLUEBERRY_ICE_CREAM_BLOCK = AbstractBlock.Properties.create(Material.SNOW_BLOCK, MaterialColor.LAPIS).harvestTool(ToolType.SHOVEL).hardnessAndResistance(0.2F).sound(SoundType.SNOW);
+
+        public static final AbstractBlock.Properties BLUEBERRY_BUSH = AbstractBlock.Properties.create(Material.PLANTS).doesNotBlockMovement().tickRandomly().hardnessAndResistance(0F).sound(SoundType.CROP);
+
+        public static final AbstractBlock.Properties BLUEBERRY_CAKE = AbstractBlock.Properties.create(Material.CAKE, MaterialColor.LAPIS).hardnessAndResistance(0.5F).sound(SoundType.CLOTH);
+
+        public static final AbstractBlock.Properties BLUEBERRY_BASKET = AbstractBlock.Properties.create(Material.WOOD, MaterialColor.LAPIS).hardnessAndResistance(1.5F).sound(SoundType.WOOD);
+        public static final AbstractBlock.Properties PINK_BLUEBERRY_BASKET = AbstractBlock.Properties.create(Material.WOOD, MaterialColor.PINK).hardnessAndResistance(1.5F).sound(SoundType.WOOD);
+    }
+}
---
+++ src/main/java/com/evoslab/why_so_blueberry/registry/WhySoBlueberryItems.java
@@ -0,0 +1,30 @@
+package com.evoslab.why_so_blueberry.registry;
+
+import com.evoslab.why_so_blueberry.WhySoBlueberry;
+import com.teamabnormals.abnormals_core.core.utils.RegistryHelper;
+import net.minecraft.item.Food;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = WhySoBlueberry.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class WhySoBlueberryItems {
+    public static final RegistryHelper HELPER = WhySoBlueberry.REGISTRY_HELPER;
+
+    //public static final RegistryObject<Item> BLUEBERRY_PIPS =
+    public static final RegistryObject<Item> BLUEBERRIES = HELPER.createItem("blueberries", ()->new Item(new Item.Properties().food(Foods.BLUEBERRIES).group(ItemGroup.FOOD)));
+
+    public static final RegistryObject<Item> PINK_BLUEBERRIES = HELPER.createItem("pink_blueberries", ()->new Item(new Item.Properties().food(Foods.PINK_BLUEBERRIES).group(ItemGroup.FOOD)));
+
+    static class Foods {
+        // temporary food values, will tweak in the future
+        public static final Food BLUEBERRIES = new Food.Builder().hunger(3).saturation(0.03F).build();
+        public static final Food PINK_BLUEBERRIES = new Food.Builder().hunger(5).saturation(0.04F).build();
+        public static final Food BLUEBERRY_ICE_CREAM = new Food.Builder().hunger(6).saturation(0.42F).build();
+        public static final Food BLUEBERRY_MILKSHAKE = new Food.Builder().hunger(3).saturation(0.6F).build();
+        public static final Food BLUEBERRY_MUFFIN = new Food.Builder().hunger(5).saturation(0.04F).build();
+        public static final Food BLUEBERRY_PIE = new Food.Builder().hunger(4).saturation(0.6F).build();
+        public static final Food BLUEBERRY_TURNOVERS = new Food.Builder().hunger(5).saturation(0.03F).build();
+    }
+}
