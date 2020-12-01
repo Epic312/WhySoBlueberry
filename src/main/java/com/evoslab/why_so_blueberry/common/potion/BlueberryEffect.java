d3eada0cb3129b19a5c4618a10b997981a0aa572
cosmicgelatin <cosmicgelatin@gmail.com>
2020-12-01 16:36:43 -0500

    Added Blueberry effect and made it reflect incoming projectiles (only for players atm)

---
+++ src/main/java/com/evoslab/why_so_blueberry/common/potion/BlueberryEffect.java
@@ -0,0 +1,11 @@
+package com.evoslab.why_so_blueberry.common.potion;
+
+import net.minecraft.potion.Effect;
+import net.minecraft.potion.EffectType;
+
+public class BlueberryEffect extends Effect {
+
+    public BlueberryEffect() {
+        super(EffectType.BENEFICIAL, 9203641);
+    }
+}
