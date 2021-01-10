0754412f0400f62e5dc68002397ffd1ccb704bcb
cosmicgelatin <cosmicgelatin@gmail.com>
2021-01-10 16:19:46 -0500

    ProjectileImpactEvent handling now makes a bounding box from the projectile's hit vector to locate entity hit

--- src/main/java/com/evoslab/canopy/core/other/CanopyEvents.java
+++ src/main/java/com/evoslab/canopy/core/other/CanopyEvents.java
@@ -3,10 +3,12 @@ package com.evoslab.canopy.core.other;
 import com.evoslab.canopy.core.Canopy;
 import com.evoslab.canopy.core.registry.CanopyEffects;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.entity.projectile.AbstractArrowEntity;
+import net.minecraft.entity.EntityPredicate;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.projectile.DamagingProjectileEntity;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.world.World;
 import net.minecraftforge.event.entity.ProjectileImpactEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -18,12 +20,10 @@ public class CanopyEvents {
     public static void onProjectileHit(ProjectileImpactEvent event) {
         if (event.getRayTraceResult().getType() == RayTraceResult.Type.ENTITY) {
             Entity entity = event.getEntity();
-            PlayerEntity target = entity.getEntityWorld().getClosestPlayer(entity, 3.0D);
-            boolean flag = true;
-            if (entity instanceof AbstractArrowEntity) {
-                flag = ((AbstractArrowEntity)entity).getPierceLevel() == 0;
-            }
-            if (flag && target != null && target.getActivePotionEffect(CanopyEffects.BLUEBERRY.get()) != null) {
+            World world = entity.getEntityWorld();
+            LivingEntity target = world.getClosestEntityWithinAABB(LivingEntity.class, EntityPredicate.DEFAULT, null, entity.getPosX(), entity.getPosY(), entity.getPosZ(), AxisAlignedBB.fromVector(event.getRayTraceResult().getHitVec()));
+
+            if (target != null && target.getActivePotionEffect(CanopyEffects.BLUEBERRY.get()) != null) {
                 if (entity.getEntityWorld().isRemote()) {
                     entity.setVelocity(entity.getMotion().getX() * -1.0D, entity.getMotion().getY() * -1.0D, entity.getMotion().getZ() * -1.0D);
                 }
